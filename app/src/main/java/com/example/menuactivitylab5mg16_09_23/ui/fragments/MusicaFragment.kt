18e60daae7e766c417f6395079ec9d76b21ec6d9
MartínRGC <martingarcia08.00@gmail.com>
2023-09-16 10:37:59 -0500

    Lab5 FRAGMENTS

---
+++ app/src/main/java/com/example/menuactivitylab5mg16_09_23/ui/fragments/MusicaFragment.kt
@@ -0,0 +1,25 @@
+package com.example.menuactivitylab5mg16_09_23.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.menuactivitylab5mg16_09_23.R
+
+
+class MusicaFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_musica, container, false)
+    }
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MenuActivitylab5MG16-09-23"
+include(":app")
